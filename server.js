7613966db0383fc6d745db890201f938385444d8
Farrell Raafi <rafi16jan@github.com>
2018-06-19 09:13:49 +0700

    Automaticly searches for modules in modules.pyj and create app.conf if not exist

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ process.chdir(__dirname)
 process.on('uncaughtException', function(error){
     console.error(error);
 });
-var conf = require('fs').readFileSync(__dirname + '/app.conf').toString()
+var fs = require('fs');
+var conf = "" +
+"master_password = yourpassword\n" +
+"admin_password = r4pyd\n" +
+"default_url = http://localhost\n" +
+"port = 8069\n" +
+"client_db = local\n" +
+"server_db = main\n" +
+"server_db_adapter = memory\n" +
+"server_db_custom_adapter = False\n" +
+"local_app = True\n" +
+"serverless = False\n"
+try {
+    if (fs.existsSync(__dirname + '/app.conf') === false) {
+        fs.writeFileSync(__dirname + '/app.conf', conf)
+    }
+    var modules_list = require('child_process').execSync('cd modules && for module in $(find */modules.pyj); do echo "import $module" | sed "s/.pyj//g" | tr / .; done', {cwd: __dirname}).toString();
+    fs.writeFileSync(__dirname + '/modules/modules.pyj', modules_list)
+} catch(error) {
+    console.log(error)
+}
+conf = fs.readFileSync(__dirname + '/app.conf').toString()
 if (conf !== '') {
     conf = conf.split(' =').join('=').split('= ').join('=').split('=').join('":"').split('\n').join('","');
     conf = '{"' + conf.slice(0, -2) + '}';
