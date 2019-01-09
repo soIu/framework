eae0c2513057e8a87d576acc064aab5eff7f48a8
Farrell Rafi <farrell@odooabc.com>
2019-01-09 09:17:21 +0700

    Make it compatible with Windows Git Bash or Cygwin

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ process.chdir(__dirname)
 process.on('uncaughtException', function(error){
     console.error(error);
 });
+var child_process = require('child_process');
+if (process.execPath.indexOf('.exe') !== -1) {
+    process.execPath = 'node';
+}
 var fs = require('fs');
 var conf = "" +
 "master_password = yourpassword\n" +
@@ -17,8 +21,8 @@ var conf = "" +
 var modules_list = '';
 var controllerst_list = '';
 try {
-    modules_list = require('child_process').execSync('cd modules && for module in $(find * -maxdepth 1 -mindepth 1 | grep modules.pyj | grep -v .pyj-cached); do echo "import $module" | sed "s/.pyj//g" | tr / .; done', {cwd: __dirname}).toString();
-    controllers_list = require('child_process').execSync('cd modules && for module in $(find * -maxdepth 1 -mindepth 1 | grep controllers.pyj | grep -v .pyj-cached); do echo "import $module" | sed "s/.pyj//g" | tr / .; done', {cwd: __dirname}).toString();
+    modules_list = child_process.execSync('cd modules && find * -maxdepth 1 -mindepth 1 | grep modules.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
+    controllers_list = child_process.execSync('cd modules && find * -maxdepth 1 -mindepth 1 | grep controllers.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
     fs.writeFileSync(__dirname + '/modules/modules.pyj', modules_list)
     fs.writeFileSync(__dirname + '/modules/controllers.pyj', controllers_list)
     if (fs.existsSync(__dirname + '/app.conf') === false) {
@@ -35,7 +39,7 @@ if (conf !== '') {
     process.env = Object.assign(process.env, conf);
 }
 //process.env.RAPYDSCRIPT_IMPORT_PATH = 'modules/';
-var command = process.execPath + ' ./node_modules/.bin/rapydscript -p modules/';
+var command = process.execPath + ' node_modules/rapydscript-ng/bin/rapydscript -p modules/';
 if (process.env.custom_modules !== undefined && process.env.custom_modules !== false) {
     //command += ':' + process.env.custom_modules + '/';
     process.env.RAPYDSCRIPT_IMPORT_PATH = process.env.custom_modules;
@@ -43,8 +47,8 @@ if (process.env.custom_modules !== undefined && process.env.custom_modules !== f
         if (fs.existsSync(process.env.custom_modules + '/__init__.pyj') === false) {
             fs.writeFileSync(process.env.custom_modules + '/__init__.pyj', '');
         }
-        modules_list += require('child_process').execSync('cd ' + process.env.custom_modules + ' && for module in $(find * -maxdepth 1 -mindepth 1 | grep modules.pyj | grep -v .pyj-cached); do echo "import $module" | sed "s/.pyj//g" | tr / .; done', {cwd: __dirname}).toString();
-        controllers_list += require('child_process').execSync('cd ' + process.env.custom_modules + ' && for module in $(find * -maxdepth 1 -mindepth 1 | grep controllers.pyj | grep -v .pyj-cached); do echo "import $module" | sed "s/.pyj//g" | tr / .; done', {cwd: __dirname}).toString();
+        modules_list += child_process.execSync('cd ' + process.env.custom_modules + ' && find * -maxdepth 1 -mindepth 1 | grep modules.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
+        controllers_list += child_process.execSync('cd ' + process.env.custom_modules + ' && find * -maxdepth 1 -mindepth 1 | grep controllers.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
         fs.writeFileSync(__dirname + '/modules/modules.pyj', modules_list);
         fs.writeFileSync(__dirname + '/modules/controllers.pyj', controllers_list);
     } catch(error) {
@@ -63,7 +67,7 @@ if (process.argv.indexOf('--serverless') === -1 && require.main === module) {
 if (process.argv.indexOf('--clear-cache') !== -1) {
     command = 'rm -f */*.pyj-cached && ' + command;
 }
-result = require('child_process').execSync(command, {cwd: __dirname, stdio: pipe, env: process.env});
+result = child_process.execSync(command, {cwd: __dirname, stdio: pipe, env: process.env});
 if (process.argv.indexOf('--serverless') !== -1 | require.main !== module) {
     if (process.argv.indexOf('--print-file') !== -1) {
        console.log(result.toString())
