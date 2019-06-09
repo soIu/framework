707547e143e85c8d7cafdbb3d750058cecc0df9c
Farrell Rafi <farrell@odooabc.com>
2019-06-09 14:43:58 +0700

    Only print error stack trace

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-process.chdir(__dirname)
-process.on('uncaughtException', function(error){
-    console.error(error);
+process.chdir(__dirname);
+process.on('uncaughtException', function(error) {
+    console.error(error.Error);
 });
 var child_process = require('child_process');
 if (process.execPath.indexOf('.exe') !== -1) {
@@ -23,15 +23,15 @@ var controllerst_list = '';
 try {
     modules_list = child_process.execSync('cd modules && find * -maxdepth 1 -mindepth 1 | grep modules.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
     controllers_list = child_process.execSync('cd modules && find * -maxdepth 1 -mindepth 1 | grep controllers.pyj | grep -v .pyj-cached | tr / . | sed "s/.pyj//g" | sed "s/^/import /"', {cwd: __dirname}).toString();
-    fs.writeFileSync(__dirname + '/modules/modules.pyj', modules_list)
-    fs.writeFileSync(__dirname + '/modules/controllers.pyj', controllers_list)
+    fs.writeFileSync(__dirname + '/modules/modules.pyj', modules_list);
+    fs.writeFileSync(__dirname + '/modules/controllers.pyj', controllers_list);
     if (fs.existsSync(__dirname + '/app.conf') === false) {
-        fs.writeFileSync(__dirname + '/app.conf', conf)
+        fs.writeFileSync(__dirname + '/app.conf', conf);
     }
 } catch(error) {
-    console.log(error)
+    console.log(error);
 }
-conf = fs.readFileSync(__dirname + '/app.conf').toString()
+conf = fs.readFileSync(__dirname + '/app.conf').toString();
 if (conf !== '') {
     conf = conf.split(' =').join('=').split('= ').join('=').split('=').join('":"').split('\n').join('","');
     conf = '{"' + conf.slice(0, -2) + '}';
@@ -58,11 +58,11 @@ if (process.env.custom_modules !== undefined && process.env.custom_modules !== f
 var pipe;
 if (process.argv.indexOf('--serverless') === -1 && require.main === module) {
     command += ' -x server.pyj';
-    pipe = 'inherit'
+    pipe = 'inherit';
 } else {
     command += ' server.pyj'
-    pipe = 'pipe'
-    process.env.serverless = true
+    pipe = 'pipe';
+    process.env.serverless = true;
 }
 if (process.argv.indexOf('--clear-cache') !== -1) {
     command = 'find . -name "*.pyj-cached" -type f -delete && ' + command;
@@ -70,8 +70,8 @@ if (process.argv.indexOf('--clear-cache') !== -1) {
 result = child_process.execSync(command, {cwd: __dirname, stdio: pipe, env: process.env});
 if (process.argv.indexOf('--serverless') !== -1 | require.main !== module) {
     if (process.argv.indexOf('--print-file') !== -1) {
-       console.log(result.toString())
-       process.exit()
+       console.log(result.toString());
+       process.exit();
     }
-    eval(result.toString())
+    eval(result.toString());
 }
