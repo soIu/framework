2f95d72fab7e8aff9b66d01c9dc9dc6792809f76
Farrell Rafi <farrell@odooabc.com>
2019-08-04 12:30:41 +0700

    Make await automaticly behaves like Promise.all if the awaited value is an array

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ if (process.argv.indexOf('--serverless') === -1) {
     vm.runInNewContext = function () {
         var args = Array.prototype.slice.call(arguments);
         if (args[2] && (args[2] === 'server.pyj' || args[2].filename === 'server.pyj')) args[0] = async_await_polyfill + (parseFloat(require('process').version.slice(1)) >= 7.6 ? args[0].toString().replace(/async\(function/g, 'async(async function').replace(/await\(/g, 'await await_all(') : args[0].toString());
-        return runInNewContext.apply(vm, arguments);
+        return runInNewContext.apply(vm, args);
     }
 }
 if (process.argv.indexOf('--print-file') !== -1 || process.argv.indexOf('--serverless') !== -1 || require.main !== module) {
