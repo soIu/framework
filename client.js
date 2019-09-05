f7e6780115c890f86f42e1988c2a8f5dd368b098
Farrell Rafi <farrell@odooabc.com>
2019-09-05 14:41:35 +0700

    Fix child process buffer

--- client.js
+++ client.js
@@ -3,5 +3,5 @@ if (process.execPath.indexOf('.exe') !== -1) {
     process.execPath = 'node';
 }
 var command = process.execPath + ' server.js --clear-cache --client';
-var client = require('child_process').execSync(command, {env: process.env}).toString();
+var client = require('child_process').spawnSync(process.execPath, ['server.js', '--clear-cache', '--client'], {env: process.env, maxBuffer: 5000 * 1024}).stdout.toString();
 process.stdout.write(client);
