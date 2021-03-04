5f40ed2b2c11a38b2298ffb790b03b5452fc62e4
Farrell Rafi <farrell@odooabc.com>
2021-03-04 14:24:48 +0700

    Dynamically create update method for each model to circumvent the static nature of RPython, and add browse method

--- orm/models.py
+++ orm/models.py
@@ -1,4 +1,5 @@
-from . import get_db
+from . import get_db, tools
+from javascript import Object, asynchronous, function
 
 class Model(object):
     _name = None
@@ -7,8 +8,49 @@ class Model(object):
     _fields = {}
 
     def __init__(self, env=False):
+        self.id = None
+        self.ids = []
         self.env = env
         self.is_env = env
+
+    def update(self, values):
+        return self
+
+    @asynchronous
+    def browse(self, id=None, ids=None):
+        if id is None and ids is None:
+           raise Exception('You have to send either id or ids as the argument')
+        singleton = id is not None
+        uuids = [id] if ids is None else ids
+        records = get_record(uuids).wait()
+        if singleton:
+           record = self._model()
+           record.id = id
+           record.ids = uuids
+           record.update(records['0'])
+        return self
+
+def get_record(ids):
+    Array = tools.get_global()['Array']
+    array = Array.new()
+    Promise = tools.get_global()['Promise']
+    promise = Promise.new(Object.createClosure(get_record_handle, Object.fromList(ids), array).toRef())
+    return promise
+
+@function
+def get_record_handle(ids, results, resolve, reject):
+    db = get_db()
+    length = ids['length']
+    closure = Object.createClosure(append_record, resolve, reject, length, results)
+    for id in ids.toArray():
+        db.get(id.toRef()).once(closure.toRef())
+
+@function
+def append_record(resolve, reject, length, results, result):
+    if result['ok'].toBoolean():
+       results['push'].call(result.toRef())
+       if results['length'].toInteger() == length.toInteger():
+          resolve(results.toRef())
 
 class Environment:
     models = {}
--- orm/tools.py
+++ orm/tools.py
@@ -1,4 +1,6 @@
 from .models import Model, Environment
+from .fields import Char
+from javascript import Object
 
 def merge(*objects, **options):
     reverse = options.get('reverse', False)
@@ -13,11 +15,62 @@ def register_models():
     for model in models:
         fields = {}
         for property in model.__dict__:
-            if property.startswith('__'): continue
+            if property.startswith('_'): continue
             value = model.__dict__[property]
             if callable(value): continue
             if type(value) == dict and all(key in value for key in ['string', 'required', 'readonly', 'protect', 'no_offline']):
                fields[property] = value
+               fields[property]['name'] = property
                setattr(model, property, value['default'])
-        model.fields = fields
+        if not fields:
+           fields['name'] = Char(string='Name')
+           fields['name']['name'] = 'name'
+        model._fields = fields
+        model._model = model
         Environment.models[model._name] = model
+        configure_model(model)
+
+def adapt_type(type):
+    if type in ['char', 'text', 'selection']:
+       return '.toString()'
+    elif type == 'integer':
+       return '.toInteger()'
+    elif type == 'float':
+       return '.toFloat()'
+    elif type == 'boolean':
+       return '.toBoolean()'
+    #Relational fields, Date, and Datetime need research
+
+model_update_template = """
+def update(self, values):
+"""
+
+def configure_model(model):
+    template = model_update_template
+    indent = ' ' * 4
+    for key in model._fields:
+        field = model._fields[key]
+        template += '\n' + indent + "if values['" + key  + "'].type != 'undefined': self." + key + ' = ' + "values['" + key + "']" + adapt_type(field['type']) + "if values['" + key + ("'].type %s else %s") % ("!= 'random'" if field['type'] in ['char', 'text', 'selection'] else "== 'number'" if field['type'] in ['integer', 'float'] else "== 'boolean'" if field['type'] == 'boolean' else "!= 'random'", field['default'] if field['type'] == 'boolean' else 0 if field['type'] == integer else 0.0 if field['type'] == 'float' else None)
+    template += '\n' + indent + 'return self'
+    namespace = {}
+    exec(template, namespace)
+    model.update = namespace['update']
+
+class Cache:
+    global_object = None
+
+cache = Cache()
+
+def get_global():
+    if cache.global_object is not None: return cache.global_object
+    window = Object.get('window')
+    if window.type != 'undefined':
+       cache.global_object = window.keep()
+       return window
+    self = Object.get('self')
+    if self.type != 'undefined':
+       cache.global_object = self.keep()
+       return self
+    require = Object.get('require').toFunction()
+    cache.global_object = require('./global.js').keep()
+    return cache.global_object
