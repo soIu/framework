19dbf0668edbe248951351108080564a18f54d51
Farrell Rafi <farrell@odooabc.com>
2021-03-20 16:03:55 +0700

    Return loaded module (for module dependency helper)

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,7 +6,7 @@ current = os.path.dirname(os.path.realpath(__file__))
 def load(module=None):
     if module:
        exec('import ' + module, globals())
-       return
+       return globals()[module]
     for dir in os.listdir(current):
         if dir[0] == '_' or '.' in dir: continue
         exec('import ' + dir, globals())
