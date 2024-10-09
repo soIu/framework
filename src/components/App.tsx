9c25bca07b295e41420917ca3ff0e5804cad001b
Farrell Rafi <farrell@odooabc.com>
2024-10-09 15:51:52 +0700

    Add viewport and add some meta webapp tags

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,12 @@ const App = async ({ name }: { name: string }) => {
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui, viewport-fit=cover"/>
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta name="format-detection" content="telephone=no"/>
+        <meta name="msapplication-tap-highlight" content="no"/>
         <title>{utils.getTitle()}</title>
       </head>
       <body>
