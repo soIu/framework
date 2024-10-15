bef01a217db8c00a36bf090f8165d3eb7b309c73
Farrell Rafi <farrell@odooabc.com>
2024-10-15 13:30:50 +0700

    Add modifiers/dependencies to make the cached value re-run (if the value is function) when the modifier is changed

--- src/utils/helpers.client.js
+++ src/utils/helpers.client.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const utils = {};
 
-utils.useCache = function useCache(value) {
+utils.useCache = function useCache(originalValue, modifiers) {
   //Cache a value in a component
-  return React.useState(value)[0];
+  const [value, setValue] = React.useState(originalValue);
+  if (!modifiers) return value;
+  React.useEffect(() => {
+    if (typeof originalValue === 'function') setValue(originalValue());
+    return () => {};
+  }, modifiers);
+  return value;
 }
 
 export default utils;
