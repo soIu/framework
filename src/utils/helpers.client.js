dbbc797ae472e88d89c97e6e05e74b5adcacd076
Farrell Rafi <farrell@odooabc.com>
2024-10-15 11:36:31 +0700

    Add useCache hook

--- src/utils/helpers.client.js
+++ src/utils/helpers.client.js
@@ -1,3 +1,10 @@
+import React from 'react';
+
 const utils = {};
+
+utils.useCache = function useCache(value) {
+  //Cache a value in a component
+  return React.useState(value)[0];
+}
 
 export default utils;
